1564e56d6444f0ce85b0bb252eac61c52c443300
Florian Schneider <schneider.f@me.com>
2024-04-02 22:55:26 +0200

    favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hi, I&apos;m Merle, a UX Designer with a decade of experience in customer service and e-commerce.</h1>
-<p>Through roles from Store Manager to Customer Service Ambassador, I've gained insights into product life cycles and building enduring customer relationships. My background in customer support and warehouse logistics has deepened my understanding of consumer needs and behaviors."</p>
+<p>Through roles from Store Manager to Customer Service Ambassador, I&apos;ve gained insights into product life cycles and building enduring customer relationships. My background in customer support and warehouse logistics has deepened my understanding of consumer needs and behaviors.</p>
     </div>
   );
 }
