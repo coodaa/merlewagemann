04f24cf963a7cac8798ab417b5b429727a13bf31
Florian Schneider <schneider.f@me.com>
2024-08-03 00:32:24 +0200

    skills css update

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,7 +1,9 @@
 // pages/about-me.js
 import React from "react";
+import Image from "next/image";
 import styles from "../styles/AboutMe.module.css";
 import SEOHead from "../components/SEOHead";
+import WaveSVG from "../components/WaveSVG";
 
 const AboutMe = () => {
   return (
@@ -9,71 +11,62 @@ const AboutMe = () => {
       <SEOHead title="About Me | Merle Wagemann" />
       <main className={styles.mainContent}>
         <h1 className={styles.title}>About Me</h1>
-        <div className={styles.profileSection}>
-          <img
-            src="/images/merlewagemann.webp"
-            alt="Merle Wagemann"
-            className={styles.profileImage}
-          />
-          <div className={styles.profileText}>
-            <p>
-              Hi, I’m Merle Wagemann, a passionate UX Designer with over a
-              decade of experience in customer service and e-commerce. My
-              journey through various roles, including Store Manager and
-              Customer Service Ambassador, has provided me with a profound
-              understanding of product life cycles and the art of building
-              long-lasting customer relationships.
-            </p>
-            <p>
-              I hold a unique analytical background in musicology, which
-              complements my skills in user experience design. My ability to
-              foster long-lasting customer relationships and excel in
-              performance marketing has consistently driven me to go the extra
-              mile.
-            </p>
-            <p>
-              Currently, I focus on enhancing digital product interactions by
-              leveraging my expertise in physical products. My aim is to create
-              seamless and intuitive user experiences that meet the needs of
-              both businesses and users.
-            </p>
+        <WaveSVG />
+        <div className={styles.likesSection}>
+          <h2>What I like</h2>
+          <div className={styles.imageCard}>
+            <Image
+              src="/images/like-twin-sister.jpg"
+              alt="spending time with my twinnie"
+              width={300}
+              height={300}
+              className={styles.image}
+            />
+            <p>spending time with my twinnie</p>
+          </div>
+          <div className={styles.imageCard}>
+            <Image
+              src="/images/like-carousel.jpg"
+              alt="riding the carousel Musikexpress"
+              width={300}
+              height={300}
+              className={styles.image}
+            />
+            <p>riding the carousel Musikexpress</p>
+          </div>
+          <div className={styles.imageCard}>
+            <Image
+              src="/images/like-tractor.jpg"
+              alt="driving a tractor with my son at Karls Erdbeerhof"
+              width={300}
+              height={300}
+              className={styles.image}
+            />
+            <p>driving a tractor with my son at Karls Erdbeerhof</p>
           </div>
         </div>
-        <div className={styles.experienceSection}>
-          <h2 className={styles.sectionTitle}>Experience</h2>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <strong>Career Foundry</strong> - Professional Development & UX
-              Certificate (Aug 2023 - Present)
-            </li>
-            <li className={styles.listItem}>
-              <strong>Berlin Candy</strong> - Co-Founder (Oct 2023 - Present)
-            </li>
-            <li className={styles.listItem}>
-              <strong>Monkind Berlin</strong> - Store Manager Fashion Retail
-              (2020 - 2023)
-            </li>
-            <li className={styles.listItem}>
-              <strong>COS Berlin</strong> - Sales Advisor & Customer Service
-              Ambassador (2012 - 2021)
-            </li>
-            <li className={styles.listItem}>
-              <strong>White Horse Music (Jung von Matt)</strong> - Intern
-              Project Management (Jun 2012 - Sept 2012)
-            </li>
-          </ul>
-        </div>
-        <div className={styles.skillsSection}>
-          <h2 className={styles.sectionTitle}>Skills</h2>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>Figma</li>
-            <li className={styles.listItem}>Wireframing</li>
-            <li className={styles.listItem}>Canva</li>
-            <li className={styles.listItem}>Prototyping</li>
-            <li className={styles.listItem}>Trello</li>
-            <li className={styles.listItem}>Miro</li>
-            <li className={styles.listItem}>Shopify</li>
-          </ul>
+        <div className={styles.dislikesSection}>
+          <h2>What I don't like</h2>
+          <div className={styles.imageCard}>
+            <Image
+              src="/images/dont-like-ladybug.jpg"
+              alt="spending time with my twinnie"
+              width={300}
+              height={300}
+              className={styles.image}
+            />
+            <p>spending time with my twinnie</p>
+          </div>
+          <div className={styles.imageCard}>
+            <Image
+              src="/images/dont-like-icecream.jpg"
+              alt="sharing my Spaghetti ice cream"
+              width={300}
+              height={300}
+              className={styles.image}
+            />
+            <p>sharing my Spaghetti ice cream</p>
+          </div>
         </div>
       </main>
     </div>
